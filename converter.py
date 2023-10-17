069891cf566d58ad9ae8886dce4937e39f8b82fc
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-18 09:46:34 +1100

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,9 +1,17 @@
-print ("welcome to the temperature and distance converter enter "1" to convert temperatures and "2" for distance")
-menuchoice == input ("enter choice")
+print ( "welcome to the temperature and distance converter enter 1 to convert temperatures and 2 for distance" )
+menuchoice = input ("enter choice: ")
 
-if menuchoice == 1:
-print ("temperature selected enter unit of temperature")
+if menuchoice == "1":
+    print ( "temperature selected enter unit of temperature C for Celcius, F for Fahrenheit and K for Kevlin " )
+    temp_unit = input ("enter selection")
+    print (temp_unit + "selected")
+    temp_value = input ("enter value of" + temp_unit)
+    print (temp_unit + temp_value + "has been chosen what would you like t convert this to?")
+    temp_convert_to = input ("what unit")
 
-else menuchoice == 2:
+elif menuchoice == "2":
+    print ( "distance selected enter unit of distance" )
+    distance_unit = input ("enter")
 
-else if:
+else:
+    endcode 
