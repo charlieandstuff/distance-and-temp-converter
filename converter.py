fa40c38710e22446791109e8ec2e686937e09f69
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-17 11:54:53 +1100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,9 @@
+print ("welcome to the temperature and distance converter enter "1" to convert temperatures and "2" for distance")
+menuchoice == input ("enter choice")
+
+if menuchoice == 1:
+print ("temperature selected enter unit of temperature")
+
+else menuchoice == 2:
+
+else if:
