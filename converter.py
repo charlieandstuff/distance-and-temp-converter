1f19a351f419bcf2e4f2851223e5ced83d59af6d
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-23 13:49:54 +1100

    Update converter.py

--- converter.py
+++ converter.py
@@ -10,8 +10,23 @@ if menuchoice == "1":
 
     if temp_unit == "C":
         output = float ((temp_value * 1.8) + 32)
-        print (f"{output} + "F")
+        print (f"{output}" + "F has been converted from" + f"{temp_value}{temp_unit}")
 
-#else menuchoice == "2":
-    #print ( "distance selected enter unit of distance " )
-   # distance_unit = input ("enter ")
+    elif temp_unit == "F":
+        output = float ((temp_value // 1.8) - 32)
+        print (f"{output}" + "C has been converted from " + f"{temp_value}{temp_unit}")
+
+    else:
+        print ("you cant follow simple steps idiot")
+        
+elif menuchoice == "2":
+    print ( "distance selected please enter what measurement you want to use " )
+    distance_unit = input ("enter ")
+    
+    temp_unit = input ("enter selection ")
+    print (temp_unit + " selected ")
+    temp_value = float (input ("enter value of " + temp_unit + " "))
+    print (f"{temp_value}{temp_unit}")
+
+else:
+        print ("you cant follow simple steps idiot")
