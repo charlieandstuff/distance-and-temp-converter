3c9c6fd0c5d9c8c637c699c0bb4d4b029832fa4a
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-24 14:28:41 +1100

    Update converter.py

--- converter.py
+++ converter.py
@@ -13,20 +13,34 @@ if menuchoice == "1":
         print (f"{output}" + "F has been converted from" + f"{temp_value}{temp_unit}")
 
     elif temp_unit == "F":
-        output = float ((temp_value // 1.8) - 32)
+        output = float ((temp_value / 1.8) - 32)
         print (f"{output}" + "C has been converted from " + f"{temp_value}{temp_unit}")
 
     else:
-        print ("you cant follow simple steps idiot")
+        print ("you cant follow simple steps idiot ")
         
 elif menuchoice == "2":
-    print ( "distance selected please enter what measurement you want to use " )
-    distance_unit = input ("enter ")
-    
-    temp_unit = input ("enter selection ")
-    print (temp_unit + " selected ")
-    temp_value = float (input ("enter value of " + temp_unit + " "))
-    print (f"{temp_value}{temp_unit}")
+    thisdict = {
+        "mm": 1,
+        "cm": 10,
+        "inch": 25.4,
+        "ft":304.8,
+        "m": 1000,
+        "km":1000000,
+        "mile":1609344
+        }
 
+    print ( "distance selected please enter what measurement you want to use  mm, cm, inch, ft, , m, km or miles " )
+    distance_unit = input ("enter ")
+    convert_from = thisdict[distance_unit]
+    print (distance_unit + " selected ")
+    distance_value = float (input ("enter value of " + distance_unit + " "))
+    print (f"{distance_value}{distance_unit}")
+    print (" please put in a unit you would like to convert to from your selected " + f"{distance_value}{distance_unit}")
+    convert_to = input ("enter unit ")
+    convert_unit = thisdict[convert_to]
+    print ("you would like to convert " + f"{distance_value}{distance_unit} " + "into " + f"{convert_to}")
+    output_distance = float((distance_value * convert_from) / convert_unit)
+    print (f"{output_distance}" + f"{convert_to}")
 else:
-        print ("you cant follow simple steps idiot")
+        print ("you cant follow simple steps idiot ")
