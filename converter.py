a86a61cd2fe5dfedc5214a080a36559251fd2880
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-26 13:36:18 +1100

    Update converter.py

--- converter.py
+++ converter.py
@@ -6,16 +6,21 @@ if menuchoice == "1":
     temp_unit = input ("enter selection ")
     print (temp_unit + " selected ")
     temp_value = float (input ("enter value of " + temp_unit + " "))
-    print (f"{temp_value}{temp_unit}")
 
-    if temp_unit == "C":
+    if temp_value.isdigit:
+       print (f"{temp_value}{temp_unit}")
+
+       if temp_unit == "C":
         output = float ((temp_value * 1.8) + 32)
         print (f"{output}" + "F has been converted from" + f"{temp_value}{temp_unit}")
 
-    elif temp_unit == "F":
+       elif temp_unit == "F":
         output = float ((temp_value / 1.8) - 32)
         print (f"{output}" + "C has been converted from " + f"{temp_value}{temp_unit}")
-        
+
+       else:
+        print ("you cant follow simple steps idiot ")
+
     else:
         print ("you cant follow simple steps idiot ")
         
