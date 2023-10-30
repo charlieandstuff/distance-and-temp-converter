412477db38d02213a66f90700f3142857ad9ae42
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-30 13:59:29 +1100

    g
    g

--- 99 bottles of beer.py
+++ 99 bottles of beer.py
@@ -1,18 +1,22 @@
 bottles_of_beer = 100
+
 while bottles_of_beer > 0:
     print (f"{bottles_of_beer}" + " bottles of beer on the wall " + f"{bottles_of_beer}" + " bottles of beer take one down pass it around ")
     bottles_of_beer = bottles_of_beer - 1
     print (f"{bottles_of_beer}" + " bottles of beer on the wall!")
+    print ("I_" * bottles_of_beer)
     
     if bottles_of_beer == 1:
        print (f"{bottles_of_beer}" + " bottle of beer on the wall " + f"{bottles_of_beer}" + " bottle of beer take one down pass it around ")
        bottles_of_beer = bottles_of_beer - 1
        print (f"{bottles_of_beer}" + " bottles of beer on the wall!")
+       print ("I_" * bottles_of_beer)
 
     if bottles_of_beer == 0:
         print (f"{bottles_of_beer}" + " bottles of beer on the wall " + f"{bottles_of_beer}" + " bottles of beer go to the store buy some more ")
         bottles_of_beer + 100
         print (f"{bottles_of_beer}" + bottles_of_beer)
+        print ("I_" * bottles_of_beer) 
         break
 
     
