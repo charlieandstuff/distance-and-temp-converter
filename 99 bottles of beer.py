847777d078158897acde743a38752f16df0354be
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-10-31 11:55:49 +1100

    Update 99 bottles of beer.py

--- 99 bottles of beer.py
+++ 99 bottles of beer.py
@@ -1,22 +1,26 @@
+import os
+import time
+
 bottles_of_beer = 100
 
 while bottles_of_beer > 0:
-    print (f"{bottles_of_beer}" + " bottles of beer on the wall " + f"{bottles_of_beer}" + " bottles of beer take one down pass it around ")
-    bottles_of_beer = bottles_of_beer - 1
-    print (f"{bottles_of_beer}" + " bottles of beer on the wall!")
-    print ("I_" * bottles_of_beer)
+    if bottles_of_beer > 1:
+        print (f"{bottles_of_beer} bottles of beer on the wall {bottles_of_beer} bottles of beer take one down pass it around ")
+        bottles_of_beer = bottles_of_beer - 1
+        print (f"{bottles_of_beer} bottles of beer on the wall!")
+        print ("I_" * bottles_of_beer)
     
     if bottles_of_beer == 1:
-       print (f"{bottles_of_beer}" + " bottle of beer on the wall " + f"{bottles_of_beer}" + " bottle of beer take one down pass it around ")
+       print (f"{bottles_of_beer} bottle of beer on the wall {bottles_of_beer} bottle of beer take one down pass it around ")
        bottles_of_beer = bottles_of_beer - 1
-       print (f"{bottles_of_beer}" + " bottles of beer on the wall!")
+       print (f"{bottles_of_beer} bottles of beer on the wall!")
        print ("I_" * bottles_of_beer)
 
     if bottles_of_beer == 0:
-        print (f"{bottles_of_beer}" + " bottles of beer on the wall " + f"{bottles_of_beer}" + " bottles of beer go to the store buy some more ")
-        bottles_of_beer + 100
-        print (f"{bottles_of_beer}" + bottles_of_beer)
-        print ("I_" * bottles_of_beer) 
+        print (f"{bottles_of_beer} bottles of beer on the wall {bottles_of_beer} bottles of beer go to the store buy some more ")
+        bottles_of_beer = bottles_of_beer + 100
+        print (f"{bottles_of_beer} bottles of beer!")       
         break
 
-    
+    time.sleep(1)
+    os.system('cls')
