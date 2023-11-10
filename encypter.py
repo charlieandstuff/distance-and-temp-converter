a0760a9c46b2c76acd71352ce92e4ba3c428fd9e
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-11-10 14:18:47 +1100

    Update encypter.py

---
+++ encypter.py
@@ -0,0 +1,25 @@
+print ("welcome to the encryption center please enter if you would like to decrypt or encrypt")
+menuchoice = input ("enter choice")
+
+if menuchoice == "encypt":
+    thisdict = {
+        "a": "n",
+        "b": "o",
+        "c": "P"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
