54cbf7a71287302dbee88f195e2d10ddb7b48b88
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-11-14 11:59:08 +1100

    gtg
    ttg

--- encypter.py
+++ encypter.py
@@ -1,25 +1,15 @@
 print ("welcome to the encryption center please enter if you would like to decrypt or encrypt")
 menuchoice = input ("enter choice")
-
-if menuchoice == "encypt":
-    thisdict = {
-        "a": "n",
-        "b": "o",
-        "c": "P"
-
+ 
+#if menuchoice == "encypt":
+yah = "hello"
+for c in yah:
+    print(ord(c))
 
 
+thisdict = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, 
+            "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23,"x": 24,"y": 25, "z": 26 
+            }
 
 
-
-
-
-
-
-
-
-
-
-
-    }
-
+alpha = "abcdefgh"
