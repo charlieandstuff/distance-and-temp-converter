910a1235a45bdac50df705ed64ea32b7f88101b3
charlieandstuff <112998001+charlieandstuff@users.noreply.github.com>
2023-12-07 14:14:10 +1100

    hhh

--- HangMan.py
+++ HangMan.py
@@ -1,32 +1,40 @@
 wordlist = ["console", "texture", "horse"]
 import random
 incorrectGuesses = 0
+correctGuesses = 0
 Hangword = (random.choice(wordlist))
 ##creates a list of that can be used to find the length of the word and replace it with an underscroe
 hiddenword = list("_" * len(Hangword))
+wincondition = 1 * len(hiddenword)
 print (" the wordlength is ")
+
 while incorrectGuesses < 6:
 
     for char in hiddenword:
         print (char, end = "")
 
-    Guess = input ("Guess! ")
+    Guess = input (" Guess! ")
 
     # If guess correct
     if Guess in Hangword:
         # Hangword remove Guess
         print ("correct!")
-        print (Guess) 
-        pos = Hangword.index(Guess)
-        print(pos)
-        # won't get all letters, need to use a loop
-        hiddenword[pos]= Guess
-    
+        print (Guess)
+        correctGuesses += 1
+        for i,char in enumerate(Hangword):
+             if Guess == char:
+                hiddenword [i] = Guess
 
-    # else if guess incorrect
+    elif correctGuesses == wincondition:
+        print ("you won the word was" + hiddenword)
+        break
+    
+    
+    # else if guess incorrect adds on to inccorect guesses then 
     else:
         print ("Wrong loser")
         incorrectGuesses += 1
+        print (f"{incorrectGuesses}" + " out of 6 incorrect guesses")
 
 else:
     print ("you lost bozo get hung")
